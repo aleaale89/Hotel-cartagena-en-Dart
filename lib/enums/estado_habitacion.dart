2e15d80d48dc7291cc7210e80e38391764aea3e1
Alejandra Bermudez <alebermudezmora@gmail.com>
2026-09-10 15:48:30 -0500

    feat: estructura modular

--- lib/enums/estado_habitacion.dart
+++ lib/enums/estado_habitacion.dart
@@ -1,7 +1,3 @@
 /// Enum que define el ciclo de estados de una habitación:
 /// disponible -> reservada -> ocupada -> disponible
-enum EstadoHabitacion {
-  disponible,
-  reservada,
-  ocupada,
-}
+
--- lib/enums/tipo_habitacion.dart
+++ lib/enums/tipo_habitacion.dart
@@ -1,6 +1,1 @@
 /// Enum que define los tipos de habitación disponibles en el hotel.
-enum TipoHabitacion {
-  simple,
-  doble,
-  suite,
-}
